b23515aec371fa345edc57060283f52f8e625665
Jixun Wu <jixun.moe@gmail.com>
2022-11-26 13:54:27 +0000

    feat: allow base64 methods to use span instead of vector

--- include/parakeet-crypto/utils/base64.h
+++ include/parakeet-crypto/utils/base64.h
@@ -7,8 +7,18 @@
 
 namespace parakeet_crypto::utils {
 
-std::vector<uint8_t> Base64Decode(const std::string& input);
-std::string Base64Encode(const std::vector<uint8_t>& input);
-std::vector<uint8_t> Base64EncodeBytes(const std::span<const uint8_t> data);
+std::string Base64Encode(const std::span<const uint8_t> input);
+inline std::vector<uint8_t> Base64EncodeBytes(const std::span<const uint8_t> data) {
+  auto result_str = Base64Encode(data);
+  return std::vector<uint8_t>(result_str.begin(), result_str.end());
+}
+
+std::vector<uint8_t> Base64Decode(const std::span<const char> input);
+inline std::vector<uint8_t> Base64Decode(const std::string& input) {
+  return Base64Decode(std::span<const char>{input});
+}
+inline std::vector<uint8_t> Base64Decode(const std::span<const uint8_t> input) {
+  return Base64Decode(std::span<const char>{reinterpret_cast<const char*>(input.data()), input.size()});
+}
 
 }  // namespace parakeet_crypto::utils
