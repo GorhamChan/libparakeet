5bafd14c08336a7c9b9a0f92a3e65afe8f94a58f
Jixun Wu <jixun.moe@gmail.com>
2022-11-26 14:17:07 +0000

    fix: allow std::span<char> for unhex

--- include/parakeet-crypto/utils/hex.h
+++ include/parakeet-crypto/utils/hex.h
@@ -14,5 +14,7 @@ std::vector<uint8_t> UnHex(const std::span<const uint8_t> hex_str);
 inline std::vector<uint8_t> UnHex(const std::string& hex_str) {
   return UnHex(std::span{reinterpret_cast<const uint8_t*>(hex_str.data()), hex_str.size()});
 }
-
+inline std::vector<uint8_t> UnHex(const std::span<const char> hex_str) {
+  return UnHex(std::span{reinterpret_cast<const uint8_t*>(hex_str.data()), hex_str.size()});
+}
 }  // namespace parakeet_crypto::utils
