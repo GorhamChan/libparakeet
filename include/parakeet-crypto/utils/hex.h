d755ee338413939fda24ae639d74c342f46d43d4
Jixun Wu <jixun.moe@gmail.com>
2022-11-26 14:07:21 +0000

    fix: make unhex available as span

--- include/parakeet-crypto/utils/hex.h
+++ include/parakeet-crypto/utils/hex.h
@@ -7,8 +7,12 @@
 
 namespace parakeet_crypto::utils {
 
-std::string Hex(const std::span<const uint8_t> v);
+std::string Hex(const std::span<const uint8_t> data);
 std::string HexCompactLowercase(const std::span<const uint8_t> data);
-std::vector<uint8_t> UnHex(const std::string& v);
+
+std::vector<uint8_t> UnHex(const std::span<const char> hex_str);
+inline std::vector<uint8_t> UnHex(const std::string& hex_str) {
+  return UnHex(std::span<const char>{hex_str});
+}
 
 }  // namespace parakeet_crypto::utils
