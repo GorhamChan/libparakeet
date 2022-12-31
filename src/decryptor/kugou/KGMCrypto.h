4a82ce3cb8685f30731ec09561c48d3431269221
Jixun Wu <jixun.moe@gmail.com>
2022-12-31 10:35:56 +0000

    docs: added notes for methods exposed in KGMCrypto.h

--- src/decryptor/kugou/KGMCrypto.h
+++ src/decryptor/kugou/KGMCrypto.h
@@ -26,6 +26,16 @@ inline uint8_t xor_u32_bytes(uint32_t value) {
 class KGMCrypto {
    public:
     virtual ~KGMCrypto() = default;
+
+    /**
+     * @brief Initialize the crypto.
+     *
+     * @param config
+     * @param slot_key
+     * @param header
+     * @return true Initialization completed without error.
+     * @return false Failed to initialize.
+     */
     virtual bool Configure(const KGMCryptoConfig& config,
                            const std::vector<uint8_t>& slot_key,
                            const kgm_file_header& header) = 0;
@@ -33,11 +43,41 @@ class KGMCrypto {
     virtual void Decrypt(uint64_t offset, std::span<uint8_t> buffer) = 0;
 };
 
-std::unique_ptr<KGMCrypto> CreateKGMCrypto(const kgm_file_header& header, const KGMCryptoConfig& config);
+/**
+ * @brief Create an initialized KGMCrypto instance based on the file header and KGMCryptoConfig.
+ *
+ * @param header
+ * @param config
+ * @return std::unique_ptr<KGMCrypto>
+ */
 std::unique_ptr<KGMCrypto> CreateKGMDecryptor(const kgm_file_header& header, const KGMCryptoConfig& config);
 
+/**
+ * @brief Create the KGMCrypto based on the header declared version, and configured with slot keys.
+ * @private
+ *
+ * @param header
+ * @param config
+ * @return std::unique_ptr<KGMCrypto>
+ */
+std::unique_ptr<KGMCrypto> CreateKGMCrypto(const kgm_file_header& header, const KGMCryptoConfig& config);
+
+/**
+ * @private
+ * @return std::unique_ptr<KGMCrypto>
+ */
 std::unique_ptr<KGMCrypto> CreateKGMCryptoType2();
+
+/**
+ * @private
+ * @return std::unique_ptr<KGMCrypto>
+ */
 std::unique_ptr<KGMCrypto> CreateKGMCryptoType3();
+
+/**
+ * @private
+ * @return std::unique_ptr<KGMCrypto>
+ */
 std::unique_ptr<KGMCrypto> CreateKGMCryptoType4();
 
 // Copy-paste of decryptor definitions...
