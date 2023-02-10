929f58fa754d2698e325dcd6c5a3d22c6caf498e
Jixun Wu <jixun.moe@gmail.com>
2023-02-10 19:38:26 +0000

    chore: add helper function to encode string to base64

--- src/utils/base64.h
+++ src/utils/base64.h
@@ -12,11 +12,15 @@ inline std::vector<uint8_t> Base64Encode(const std::vector<uint8_t> &data)
 {
     return Base64Encode(data.data(), data.size());
 }
+inline std::vector<uint8_t> Base64Encode(const std::string &data)
+{
+    return Base64Encode(reinterpret_cast<const uint8_t *>(data.c_str()), data.size()); // NOLINT(*-reinterpret-cast)
+}
 
 std::vector<uint8_t> Base64Decode(const uint8_t *input, size_t len);
 inline std::vector<uint8_t> Base64Decode(const std::string &data)
 {
-    return Base64Decode(reinterpret_cast<const uint8_t *>(data.data()), data.size()); // NOLINT(*-type-reinterpret-cast)
+    return Base64Decode(reinterpret_cast<const uint8_t *>(data.data()), data.size()); // NOLINT(*-reinterpret-cast)
 }
 inline std::vector<uint8_t> Base64Decode(const std::vector<uint8_t> &data)
 {
