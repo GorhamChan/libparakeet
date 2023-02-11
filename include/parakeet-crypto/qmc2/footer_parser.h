a04c8e4b3a8778cd1272680dca4e5babb46681ce
Jixun Wu <jixun.moe@gmail.com>
2023-02-11 00:18:26 +0000

    feat: add helper meethod to parse qmc2 footer from stream

--- include/parakeet-crypto/qmc2/footer_parser.h
+++ include/parakeet-crypto/qmc2/footer_parser.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "key_crypto.h"
+#include "parakeet-crypto/IStream.h"
 
 #include <cstddef>
 #include <cstdint>
@@ -17,6 +18,7 @@ enum class FooterParseState
     NeedMoreBytes = 1,
     KeyDecryptionFailure = 2,
     UnsupportedAndroidClientSTag = 3,
+    IOReadFailure = 4,
     UnknownContent = 9,
 };
 
@@ -57,6 +59,38 @@ class QMCFooterParser
     virtual ~QMCFooterParser() = default;
 
     virtual std::unique_ptr<FooterParseResult> Parse(const uint8_t *file_footer, size_t len) = 0;
+
+    inline std::unique_ptr<FooterParseResult> Parse(IReadSeekable &input_stream)
+    {
+        constexpr size_t kInitialFooterSize = 1024;
+        auto initial_seek_pos = input_stream.GetOffset();
+
+        input_stream.Seek(-kInitialFooterSize, SeekDirection::FILE_END_BACKWARDS);
+        std::vector<uint8_t> footer_buffer(kInitialFooterSize, 0);
+
+        // In case of partial read (file too small?), keep track of the numuber of bytes available.
+        auto bytes_read = input_stream.Read(footer_buffer.data(), footer_buffer.size());
+
+        // 1st attempt to parse it
+        auto footer = Parse(footer_buffer.data(), bytes_read);
+        if (footer->state == qmc2::FooterParseState::NeedMoreBytes)
+        {
+            // We need more data, resize our buffer.
+            footer_buffer.resize(footer->footer_size);
+            input_stream.Seek(-footer->footer_size, SeekDirection::FILE_END_BACKWARDS);
+            if (!input_stream.ReadExact(footer_buffer.data(), footer_buffer.size()))
+            {
+                input_stream.Seek(initial_seek_pos, SeekDirection::FILE_BEGIN);
+                return std::make_unique<FooterParseResult>(FooterParseState::IOReadFailure, footer_buffer.size());
+            }
+
+            // 2nd attempt to parse it
+            footer = Parse(footer_buffer.data(), footer->footer_size);
+        }
+
+        input_stream.Seek(initial_seek_pos, SeekDirection::FILE_BEGIN);
+        return footer;
+    }
 };
 
 std::unique_ptr<QMCFooterParser> CreateQMC2FooterParser(std::shared_ptr<IKeyCrypto> key_crypto);
